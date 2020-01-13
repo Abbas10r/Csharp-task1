85145072f513aea1966738279966d746d7a846ca
Abbas Azisov <abbastktl@gmail.com>
2020-01-13 14:32:19 +0600

    Commit1

---
+++ App1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Fuck The World!";
+            Console.WriteLine("Fuck The World!");
+            Console.WriteLine($"{str}");
+        }
+    }
+}
